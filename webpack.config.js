3dab6b235214148f9bc80e82175f4e9bc2037559
randallwstanford <randallwstanford@gmail.com>
2021-12-22 00:59:41 -0700

    Adds .tx and .tsx to be complied.

--- webpack.config.js
+++ webpack.config.js
@@ -18,9 +18,9 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env', '@babel/preset-react', '@babel/typescript'],
-          }
-        }
-      }
-    ]
-  }
+          },
+        },
+      },
+    ],
+  },
 };
