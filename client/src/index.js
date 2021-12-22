40dda2f0b75b6fa40240c97fc3f7f26360b85079
randallwstanford <randallwstanford@gmail.com>
2021-12-21 22:41:03 -0700

    Creates App, Home, and Index components.

---
+++ client/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Components
+import App from './components/App.jsx';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'),
+);
