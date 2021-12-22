6dc87a3699953e4ea470718ec40603eb1c037d06
randallwstanford <randallwstanford@gmail.com>
2021-12-21 22:45:21 -0700

    Changes SRC and DIST dirs.

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, '/client/src/index.js');
+const DIST_DIR = path.join(__dirname, '/client/dist/');
+
+module.exports = {
+  entry: SRC_DIR,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js(x?)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          }
+        }
+      }
+    ]
+  }
+};
