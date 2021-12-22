c73aa11c0c364a68ed8bd7a41f06337f5adf8e55
randallwstanford <randallwstanford@gmail.com>
2021-12-21 22:37:21 -0700

    Sets express static to correct file path.

---
+++ server/index.ts
@@ -0,0 +1,25 @@
+import express, { Express, Request, Response } from "express";
+import path from 'path';
+
+export class Server {
+
+  private app: Express;
+
+  constructor(app: Express) {
+    this.app = app;
+
+    this.app.use(express.static(path.join(__dirname, '../', 'client', 'dist')));
+
+    this.app.get("/api", (req: Request, res: Response): void => {
+        res.send("You have reached the API!");
+    });
+
+    this.app.get("*", (req: Request, res: Response): void => {
+      res.sendFile(path.resolve("./") + "/client/dist/index.html");
+    });
+}
+
+  public start(port: number): void {
+    this.app.listen(port, () => console.log(`🧠 Server listening on port ${port}!`));
+  };
+}
