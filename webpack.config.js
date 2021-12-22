4829e7e0e535792f219a42f7f9f735a4f4bf1937
randallwstanford <randallwstanford@gmail.com>
2021-12-22 00:58:47 -0700

    Adds .ts and .tsx extensions to be compiled.

--- webpack.config.js
+++ webpack.config.js
@@ -12,12 +12,12 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js(x?)$/,
+        test: /\.(js|mjs|jsx|ts|tsx)$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
+            presets: ['@babel/preset-env', '@babel/preset-react', '@babel/typescript'],
           }
         }
       }
