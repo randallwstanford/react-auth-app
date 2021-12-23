5b15067044fa50b66ec95507f2cf8d4610e6efdc
randallwstanford <randallwstanford@gmail.com>
2021-12-22 19:17:47 -0700

    Refactor functions and routing

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home.tsx';
+import Login from './Login.tsx';
+import useToken from './useToken';
 
-const App = function () {
+const App = () => {
+  const { token, setToken } = useToken();
+
+  if (!token) return <Login setToken={setToken} />;
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <h1>Home page</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
   );
 };
 
