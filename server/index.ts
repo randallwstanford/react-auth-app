bab1bc0ef5c8f4988e2b1e930905bb67ae9cb7a8
randallwstanford <randallwstanford@gmail.com>
2021-12-22 19:21:36 -0700

    Simplifies server file structure.

--- server/index.ts
+++ server/index.ts
@@ -1,35 +1,8 @@
-/* eslint-disable no-console */
-import express, { Express, Request, Response } from 'express';
-import cors from 'cors';
-import path from 'path';
+import express from 'express';
+import Server from './server';
 
-export default class Server {
-  private app: Express;
+const app = express();
+const port = 3000;
+const server = new Server(app);
 
-  constructor(app: Express) {
-    this.app = app;
-
-    this.app.use(express.static(path.join(__dirname, '../', 'client', 'dist')));
-
-    // Enable Cors
-    this.app.use((req, resp, next) => next(), cors({ maxAge: 84600 }));
-
-    this.app.use('/login', (req, res) => {
-      res.send({
-        token: 'test123',
-      });
-    });
-
-    this.app.get('/api', (req: Request, res: Response): void => {
-      res.send('You have reached the API!');
-    });
-
-    this.app.get('*', (req: Request, res: Response): void => {
-      res.sendFile(`${path.resolve('./')}/client/dist/index.html`);
-    });
-  }
-
-  public start(port: number): void {
-    this.app.listen(port, () => console.log(`🧠 Server listening on port ${port}!`));
-  }
-}
+server.start(port);
