79e22af915d9fd3c929b9584167f0a94b7a39f9e
randallwstanford <randallwstanford@gmail.com>
2021-12-22 19:13:40 -0700

    Adds cors and token middleware.

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,6 @@
 /* eslint-disable no-console */
 import express, { Express, Request, Response } from 'express';
+import cors from 'cors';
 import path from 'path';
 
 export default class Server {
@@ -9,6 +10,15 @@ export default class Server {
     this.app = app;
 
     this.app.use(express.static(path.join(__dirname, '../', 'client', 'dist')));
+
+    // Enable Cors
+    this.app.use((req, resp, next) => next(), cors({ maxAge: 84600 }));
+
+    this.app.use('/login', (req, res) => {
+      res.send({
+        token: 'test123',
+      });
+    });
 
     this.app.get('/api', (req: Request, res: Response): void => {
       res.send('You have reached the API!');
